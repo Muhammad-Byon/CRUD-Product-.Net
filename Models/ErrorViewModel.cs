3acc943d000c8e5b847e3e7cdafe4783cff4d9cc
muhammad <muhammadbyon31@gmail.com>
2024-07-08 18:01:43 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UASWebApp2001092011.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
